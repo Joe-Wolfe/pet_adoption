bb120906385bb4bfaa00e8574050e07f95451be0
Joe Wolfe <JoeWolfe0527@gmail.com>
2023-11-23 12:47:32 -0500

    added an edit page for pets and a requirements.txt file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, request, render_template, redirect
 from models import db, connect_db, Pet
-from forms import PetForm
+from forms import PetForm, EditPetForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adoption_db'
@@ -10,18 +10,19 @@ app.config['SECRET_KEY'] = 'secret'
 app.app_context().push()
 
 connect_db(app)
-db.drop_all()
 db.create_all()
 
 
 @app.route('/')
 def home():
+    """Show homepage with list of pets."""
     pets = Pet.query.all()
     return render_template('index.html', pets=pets)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_pet():
+    """Show add pet form and handle form submission."""
     form = PetForm()
     if form.validate_on_submit():
         name = form.name.data
@@ -39,6 +40,23 @@ def add_pet():
         return redirect('/')
     else:
         return render_template('add_pet.html', form=form)
+
+
+@app.route('/<int:pet_id>', methods=['GET', 'POST'])
+def show_pet(pet_id):
+    """Show details about a pet."""
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+    print(form.errors)
+    print(form.validate_on_submit())
+    if form.validate_on_submit():
+        pet.photo_url = form.photo_url.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('pet_details.html', pet=pet, form=form)
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,15 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField
-from wtforms.validators import InputRequired, Length, NumberRange, Optional, URL, DataRequired, Regexp
+from wtforms.validators import InputRequired, Length, NumberRange, Optional, URL, DataRequired, AnyOf
 
 
 class PetForm(FlaskForm):
     """Form for adding pets."""
     name = StringField("Pet Name", validators=[
                        InputRequired(), DataRequired(), Length(min=2, max=100)])
-    species = StringField("Species", validators=[
+    species = StringField("Species", filters=[lambda s: s.lower() if s else s], validators=[AnyOf(['cat', 'dog', 'porcupine'], message="Must be a cat, dog, or porcupine"),
                           InputRequired(), DataRequired(), Length(min=2, max=100)])
     photo_url = StringField('Photo URL', validators=[Optional(), URL()])
-    age = IntegerField("Age", validators=[NumberRange(min=0, max=30)])
+    age = IntegerField("Age", validators=[
+                       NumberRange(min=0, max=30), Optional()])
+    notes = StringField("Notes")
+    available = BooleanField("Available")
+
+
+class EditPetForm(FlaskForm):
+    """Form for editing pets."""
+    photo_url = StringField('Photo URL', validators=[Optional(), URL()])
     notes = StringField("Notes")
     available = BooleanField("Available")
